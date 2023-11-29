eefdb3a6038dffe7aa743fd6f3334b87d1fa842d
quang <quanglatoi002@gmail.com>
2023-11-29 18:14:09 +0700

    public link S3 aws out community

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -1,19 +1,40 @@
 "use strict";
 
-const { s3, PutObjectCommand } = require("../configs/s3.config");
+const {
+    s3,
+    PutObjectCommand,
+    GetObjectCommand,
+    DeleteBucketCommand,
+} = require("../configs/s3.config");
 const crypto = require("crypto");
+
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+
+const randomImageName = () => crypto.randomBytes(16).toString("hex");
+
 const updateImageFromLocalS3 = async ({ file }) => {
     try {
-        const randomImageName = () => crypto.randomBytes(16).toString("hex");
+        // start update image up s3
+        const imageName = randomImageName();
         const command = new PutObjectCommand({
             Bucket: process.env.AWS_BUCKET_NAME,
-            Key: randomImageName() || "unknown",
+            Key: imageName,
             Body: file.buffer,
             ContentType: "image/jpeg",
         });
 
+        //export url
         const result = await s3.send(command);
-        return result;
+
+        //end update
+        //start public link image out community
+        const singedUrl = new GetObjectCommand({
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: imageName, //file.originalname
+        });
+        const url = await getSignedUrl(s3, singedUrl, { expiresIn: 3600 });
+
+        return url;
         // return {
         //     images_url,
         //     shopId,
